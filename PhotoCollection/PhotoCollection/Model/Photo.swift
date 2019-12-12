b6127d6d1b8d530c56cfe311d829d394193c933d
Gerardo Hernandez <gahernandez02@gmail.com>
2019-12-12 12:23:11 -0800

    Initial commmit. Added Photo Model

---
+++ PhotoCollection/PhotoCollection/Model/Photo.swift
@@ -0,0 +1,14 @@
+//
+//  Photo.swift
+//  PhotoCollection
+//
+//  Created by Gerardo Hernandez on 12/12/19.
+//  Copyright © 2019 Gerardo Hernandez. All rights reserved.
+//
+
+import Foundation
+
+struct Photo: Equatable {
+    var imageData: Data
+    var title: String
+}
