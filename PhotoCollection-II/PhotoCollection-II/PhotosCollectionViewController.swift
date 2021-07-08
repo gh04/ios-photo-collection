f420091bc22f4d0cdf99b8a71401c8d59fd1f909
Gerardo Hernandez <gahernandez02@gmail.com>
2021-07-08 12:23:17 -0700

    Deleted boilerplate code and added place comment marks

--- PhotoCollection-II/PhotoCollection-II/PhotosCollectionViewController.swift
+++ PhotoCollection-II/PhotoCollection-II/PhotosCollectionViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class PhotosCollectionViewController: UICollectionViewController {
     
+    // MARK: - Properties
     let photoContoller = PhotoController()
     let themeHelper = ThemeHelper()
 
@@ -17,7 +18,7 @@ class PhotosCollectionViewController: UICollectionViewController {
         super.viewDidLoad()
         
     }
-    
+    // MARK: - Method
     func setTheme() {
         guard let currentTheme = themeHelper.themePreference else { return }
         self.collectionView.backgroundColor = UIColor.colorWith(name: currentTheme)
@@ -62,39 +63,8 @@ class PhotosCollectionViewController: UICollectionViewController {
         return cell
     }
 
-    // MARK: UICollectionViewDelegate
-
-    /*
-    // Uncomment this method to specify if the specified item should be highlighted during tracking
-    override func collectionView(_ collectionView: UICollectionView, shouldHighlightItemAt indexPath: IndexPath) -> Bool {
-        return true
-    }
-    */
-
-    /*
-    // Uncomment this method to specify if the specified item should be selected
-    override func collectionView(_ collectionView: UICollectionView, shouldSelectItemAt indexPath: IndexPath) -> Bool {
-        return true
-    }
-    */
-
-    /*
-    // Uncomment these methods to specify if an action menu should be displayed for the specified item, and react to actions performed on the item
-    override func collectionView(_ collectionView: UICollectionView, shouldShowMenuForItemAt indexPath: IndexPath) -> Bool {
-        return false
-    }
-
-    override func collectionView(_ collectionView: UICollectionView, canPerformAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) -> Bool {
-        return false
-    }
-
-    override func collectionView(_ collectionView: UICollectionView, performAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) {
-    
-    }
-    */
-
 }
-
+// MARK: - Extension
 extension UIColor {
 
     static func colorWith(name:String) -> UIColor? {
--- PhotoCollection-II/PhotoCollection-II/Storyboards/ThemeSelectionViewController.swift
+++ PhotoCollection-II/PhotoCollection-II/Storyboards/ThemeSelectionViewController.swift
@@ -31,17 +31,5 @@ class ThemeSelectionViewController: UIViewController {
     @IBAction func cancel(_ sender: UIButton) {
         self.dismiss(animated: true, completion: nil)
     }
-    
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
