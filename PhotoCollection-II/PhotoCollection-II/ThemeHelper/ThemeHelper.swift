a5c0d9f957bd0151c93565be58d6442986b22a9d
Gerardo Hernandez <gahernandez02@gmail.com>
2021-07-08 09:49:34 -0700

    Created and implemented the ThemeHelper class

--- PhotoCollection-II/PhotoCollection-II/ThemeHelper/ThemeHelper.swift
+++ PhotoCollection-II/PhotoCollection-II/ThemeHelper/ThemeHelper.swift
@@ -6,3 +6,46 @@
 //
 
 import Foundation
+
+enum Theme: String, CustomStringConvertible {
+    
+    var description: String {
+        return rawValue
+    }
+    
+    case dark = "Dark"
+    case blue = "Blue"
+    case light = "Light"
+}
+
+class ThemeHelper {
+    
+    init() {
+        if themePreference == nil {
+            setThemePreferenceToLight()
+        }
+    }
+    
+    func setThemePreferenceToDark() {
+        UserDefaults.standard.set(Theme.dark.rawValue, forKey: .themePreferenceKey)
+    }
+    
+    func setThemePreferenceToBlue() {
+        UserDefaults.standard.set(Theme.blue.rawValue, forKey: .themePreferenceKey)
+    }
+    
+    func setThemePreferenceToLight() {
+        UserDefaults.standard.set(Theme.light.rawValue, forKey: .themePreferenceKey)
+    }
+    
+    var themePreference: String?  {
+        let rawValue = UserDefaults.standard.string(forKey: .themePreferenceKey).flatMap(Theme.init) ?? .light
+        return rawValue.description
+    }
+}
+
+extension String {
+    static var themePreferenceKey = "ThemePreference"
+}
+
+
