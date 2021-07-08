a639b0d8ef2e032cea311ef7a870c90e1ed5faec
Gerardo Hernandez <gahernandez02@gmail.com>
2021-07-08 11:29:33 -0700

    Implemented PhotoCollection cell class

--- PhotoCollection-II/PhotoCollection-II/Storyboards/PhotosCollectionViewCell.swift
+++ PhotoCollection-II/PhotoCollection-II/Storyboards/PhotosCollectionViewCell.swift
@@ -8,5 +8,24 @@
 import UIKit
 
 class PhotosCollectionViewCell: UICollectionViewCell {
+
+    // MARK: - Outlets
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var label: UILabel!
     
+    // MARK: Properties
+    private var photo: Photo? {
+        didSet {
+            updateViews()
+        }
+    }
+    
+    
+private func updateViews() {
+    guard let photo = photo else { return }
+    
+    imageView.image = UIImage(data: photo.imageData)
+    label.text = photo.title
+
+    }
 }
