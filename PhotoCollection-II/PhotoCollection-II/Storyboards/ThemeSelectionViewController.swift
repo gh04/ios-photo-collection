95c7063cd3979e2de8173624742f32e6eb501fed
Gerardo Hernandez <gahernandez02@gmail.com>
2021-07-08 11:06:15 -0700

    Set up ThemeSelectionVC scene in storyboard and connected outlets

--- PhotoCollection-II/PhotoCollection-II/Storyboards/ThemeSelectionViewController.swift
+++ PhotoCollection-II/PhotoCollection-II/Storyboards/ThemeSelectionViewController.swift
@@ -9,11 +9,29 @@ import UIKit
 
 class ThemeSelectionViewController: UIViewController {
 
+    // MARK: - Properties
+    var themeHelper: ThemeHelper?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
+    
+    // MARK: - Actions
+    @IBAction func selectDarkThemeButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func selectLightThemeButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func selectBlueThemeButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func cancel(_ sender: UIButton) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
     
 
     /*
