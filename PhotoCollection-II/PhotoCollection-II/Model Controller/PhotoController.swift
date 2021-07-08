2ed620580c805085676a3689fdd3fb21c10a6a08
Gerardo Hernandez <gahernandez02@gmail.com>
2021-07-08 10:42:45 -0700

    Implemented the update method in the PhotoController class

--- PhotoCollection-II/PhotoCollection-II/Model Controller/PhotoController.swift
+++ PhotoCollection-II/PhotoCollection-II/Model Controller/PhotoController.swift
@@ -17,6 +17,11 @@ class PhotoController {
         photos.append(photo)
     }
     private func update(photo: Photo, imageData: Data, title: String) {
+        // Getting the first instance of the photo we pass in
+        guard let index = photos.firstIndex(of: photo) else { return }
         
+        let updatedPhoto = Photo(imageData: imageData, title: title)
+        // Assiging the updated photo into our array of photos
+        photos[index] = updatedPhoto
     }
 }
