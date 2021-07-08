77b8c605dd21c08c64f375d69bc9f739060d721c
Gerardo Hernandez <gahernandez02@gmail.com>
2021-07-08 08:24:36 -0700

    Added cocoa touch and swift files to the project as well as empty view controllers scenes in storyboard

---
+++ PhotoCollection-II/PhotoCollection-II/Storyboards/PhotosCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  PhotosCollectionViewCell.swift
+//  PhotoCollection-II
+//
+//  Created by Gerardo Hernandez on 7/8/21.
+//
+
+import UIKit
+
+class PhotosCollectionViewCell: UICollectionViewCell {
+    
+}
